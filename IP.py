436459103683c0e5b8ee828aa1de2716b0345806
Solomone0 <96940308+Solomone0@users.noreply.github.com>
2022-01-01 21:10:59 +0200

    Update IP.py

--- IP.py
+++ IP.py
@@ -1,25 +1,32 @@
-
-
-
 import os
-
 os.system("clear")
-
 import socket
-
-print("="*20)
-
+print("""____   _______   _____   _______     -------
+  / __ \ |__   __| /   _ \ |__   __|  -====------
+ | (__) |   | |    \  \ \_\   | |    -======------
+ |  __  |   | |    /   \ __   | |    --====-------
+ | |  | |   | |   |  (\ / /   | |     -----------
+ |_|  |_|   |_|    \_____/    |_|       -------""")
+print("  ")
+print("  ")
+a="Enter the number one or two number"
+print(a.center(60))
+print("  ")
+print("1= Know IP \n2= Know the IP reference")
+print(" ")
+print(" ")
+a=int(input("Enter the number one or twe : "))
 while True:
-
-    a=input("Enter Your IP name : ")
-
-    ip=socket.gethostbyname(a)
-
-    print(a," = " ,ip)
-
-
-
-
-
-
-
+		if a==1:
+			m=input("Enter Your name >! ")
+			if m=='done':
+				break
+			ip=socket.gethostbyname(m)
+			print(ip)
+		elif a==2:
+			p=input("Enter the ip >! : ")
+			if p=='done':
+				break
+			host=socket.gethostbyaddr(str(p))
+			print(host)
+		
