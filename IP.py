7e27530cf6ce3787070c63e5ab6208128422e7ee
Solomone0 <96940308+Solomone0@users.noreply.github.com>
2022-01-01 15:11:18 +0300

    Update IP.py

--- IP.py
+++ IP.py
@@ -1,4 +1,8 @@
+import os
+os.system("clear")
 import socket
-a=input("Enter Your IP name : ")
-ip=socket.gethostbyname(a)
-print(ip)
+print("="*20)
+while True:
+    a=input("Enter Your IP name : ")
+    ip=socket.gethostbyname(a)
+    print(ip)
